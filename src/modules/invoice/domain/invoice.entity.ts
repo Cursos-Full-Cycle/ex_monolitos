1e284e2e1699a55a6dd38fd5610d267c574a0127
Fabio de Stefani <fabiostefani@gmail.com>
2023-07-04 06:32:04 -0300

    Started module Invoice

---
+++ src/modules/invoice/domain/invoice.entity.ts
@@ -0,0 +1,44 @@
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface";
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Product from "./product.entity";
+
+type InvoiceProps = {
+    id?: Id;
+    name: string;
+    document: string;
+    address: string;
+    items: Product[];
+};
+
+export default class Invoice extends BaseEntity implements AggregateRoot {
+    private _name: string;    
+    private _document: string;
+    private _address: string;// value object
+    private _items: Product[]; // Product entity
+
+    constructor(props: InvoiceProps) {
+        super(props.id);
+        this._name = props.name;
+        this._document = props.document;
+        this._address = props.address;
+        this._items = props.items;        
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get document(): string {
+        return this._document;
+    }
+
+    get address(): string {
+        return this._address;
+    }
+
+    get items(): Product[] {
+        return this._items;
+    }
+    
+}
