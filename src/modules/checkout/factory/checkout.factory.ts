83b1834995a9d4a980bd954ac47051983deb12e3
Fabio de Stefani <fabiostefani@gmail.com>
2023-07-18 08:00:46 -0300

    checkout facade

---
+++ src/modules/checkout/factory/checkout.factory.ts
@@ -0,0 +1,58 @@
+import ClientAdmFacade from "../../client-adm/facade/client-adm.facade";
+import ClientRepository from "../../client-adm/repository/client.repository";
+import AddClientUseCase from "../../client-adm/usecase/add-client/add-client.usecase";
+import FindClientUseCase from "../../client-adm/usecase/find-client/find-client.usecase";
+import InvoiceFacade from "../../invoice/facade/invoice.facade";
+import InvoiceRepository from "../../invoice/repository/invoice/invoice.repository";
+import FindInvoiceUseCase from "../../invoice/usecase/find-invoice/find-invoice.usecase";
+import GenerateInvoiceUseCase from "../../invoice/usecase/generate-invoice/generate-invoice.usecase";
+import PaymentFacade from "../../payment/facade/payment.facade";
+import TransactionRepository from "../../payment/repository/transaction.repository";
+import ProcessPaymentUseCase from "../../payment/usecase/process-payment/process-payment.usecase";
+import ProductAdmFacade from "../../product-adm/facade/product-adm.facade";
+import ProductRepository from "../../product-adm/repository/product.repository";
+import AddProductUseCase from "../../product-adm/usecase/add-product/add-product.usecase";
+import StockProductUseCase from "../../product-adm/usecase/stock-product/stock-product.usecase";
+import StoreCatalogFacade from "../../store-catalog/facade/store-catalog.facade";
+import FindProductUseCase from "../../store-catalog/usecase/find-product/find-product.usecase";
+import OrderRepository from "../repository/order/order.repository";
+
+export default class CheckoutFacadeFactory {
+    static create() {
+        const orderRepository = new OrderRepository();        
+        const clientRepository = new ClientRepository();
+        const findClientUseCase = new FindClientUseCase(clientRepository);
+        const addClientUseCase = new AddClientUseCase(clientRepository);
+        const clientFacade = new ClientAdmFacade({
+            findUseCase: findClientUseCase,
+            addUseCase: addClientUseCase,
+        });
+
+        const productAdmRepository = new ProductRepository();
+        const addProductUseCase =  new AddProductUseCase(productAdmRepository);
+        const checkStockUseCase = new StockProductUseCase(productAdmRepository);
+        const productFacade = new ProductAdmFacade({
+            addUseCase: addProductUseCase,
+            stockUseCase: checkStockUseCase,
+        });
+        
+        // const productStoreCatalogRepository = new ProductRepository();
+        // const findUseCase = new FindProductUseCase(productRepository);
+        // findAllUseCase: FindAllProductsUseCase
+        // const catalogFacade = new StoreCatalogFacade({
+
+        // });
+        const invoiceRepository = new InvoiceRepository();
+        const find = new FindInvoiceUseCase(invoiceRepository);
+        const generate = new GenerateInvoiceUseCase(invoiceRepository);
+        const invoiceFacade = new InvoiceFacade({
+            find: find,
+            generate: generate,
+        });
+        const transactionRepository = new TransactionRepository();
+        const processPaymentUseCase = new ProcessPaymentUseCase(transactionRepository);
+        const paymentFacade = new PaymentFacade(processPaymentUseCase);
+
+        return invoiceFacade;
+    }
+}
